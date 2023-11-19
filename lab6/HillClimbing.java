caedfbb837fcc9fc6d54f3cb66c62abdc5af691c
duytannlu <144307798+duytannlu@users.noreply.github.com>
2023-11-19 13:51:32 +0700

    lab 6

---
+++ lab6/HillClimbing.java
@@ -0,0 +1,15 @@
+package lab6;
+
+import lab7.Node;
+
+public class HillClimbing {
+	public Node execute(Node initialState) {
+		
+		return null;
+	}
+
+	public Node executeHillClimbingWithRandomRestart(Node initialState) {
+		// Enter your code here.
+		return null;
+	}
+}
